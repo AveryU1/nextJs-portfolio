986a8e601cd9620b464c4a98206a98ac9c4f125d
Cristopher <criss_19992016@outlook.com>
2023-07-08 15:30:05 -0400

    first commit

---
+++ public/assets/index.js
@@ -0,0 +1,49 @@
+import about01 from "./about01.webp";
+import about02 from "./about02.webp";
+import about03 from "./about03.webp";
+import avatar from "./avatar.webp";
+import css from "./css.webp";
+import ecommerce from "./ecommerce.webp";
+import email from "./email.webp";
+import eventsLandingPage from "./events-landing-page.webp";
+import github from "./github.webp";
+import html from "./html.webp";
+import logo from "./logo.webp";
+import modernBank from "./modern-bank.webp";
+import phone from "./phone.webp";
+import portfolio from "./portfolio.webp";
+import react from "./react.webp";
+import sass from "./sass.webp";
+import space from "./space.webp";
+import tailWindCSS from "./tailwind-css-logo.png";
+import trueBeauty from "./true-beauty-landing-page.webp";
+import typescript from "./typescript.webp";
+import skill from "./skill.webp";
+import user from "./user.webp";
+import view from "";
+
+export {
+    about01,
+    about02,
+    about03,
+    avatar,
+    css,
+    ecommerce,
+    email,
+    eventsLandingPage,
+    github,
+    html,
+    logo,
+    modernBank,
+    phone,
+    portfolio,
+    react,
+    sass,
+    space,
+    tailWindCSS,
+    trueBeauty,
+    typescript,
+    skill,
+    user,
+    view,
+};
